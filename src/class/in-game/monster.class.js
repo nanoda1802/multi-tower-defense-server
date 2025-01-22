bb54b5d16d1917008574e9663eef8a2af6569a00
kimjehoon <kms5064@naver.com>
2025-01-22 14:35:45 +0900

    [feat] monster.class
    1. monster.class 초안 생성

--- src/class/in-game/monster.class.js
+++ src/class/in-game/monster.class.js
@@ -1,3 +1,15 @@
-class Monster {}
+import monsterData from '../../assets/monster.js'
+
+class Monster {
+    constructor(monsterId, monsterNumber, level, Rcode) {
+      this.monsterId = monsterId;
+      this.monsterNumber = monsterNumber;
+      this.level = level;
+      this.stat = monsterData.find((element) => element.Rcode === Rcode);
+      this.type = ''; //'own', 'enemy' 로 표기
+    }
+  
+  }
+  
 
 export default Monster;
