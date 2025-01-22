511a2cd22b9a2079394624e4302ab93647b15582
bakfox <153157605+bakfox@users.noreply.github.com>
2025-01-22 14:49:42 +0900

    [feat] 룸 기본 데이터 추가
    1.기본 데이터 추가
    2.기본 데이터 받아오기 추가 객체로 받음

--- src/class/room/room.class.js
+++ src/class/room/room.class.js
@@ -2,7 +2,13 @@ import Player from '../in-game/player.class';
 
 class Room {
   constructor(id, users) {
-    id = id;
+    this.id = id;
+    // 상수들 입니다
+    this.baseHp = 10;
+    this.towerCost = 10;
+    this.initialGold = 10;
+    this.monsterSpawnInterval = 1;
+
     this.players = new Map();
     setPlayers(users);
   }
@@ -17,6 +23,14 @@ class Room {
     if (this.players[socket] !== undefined) {
       return this.players[socket];
     }
+  }
+  getInitData() {
+    return {
+      baseHp: this.baseHp,
+      towerCost: this.towerCost,
+      initialGold: this.initialGold,
+      monsterSpawnInterval: this.monsterSpawnInterval,
+    };
   }
 }
 
