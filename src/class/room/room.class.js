f6d6c646638a58b163f3f62ec547e5e7035e9bab
bakfox <153157605+bakfox@users.noreply.github.com>
2025-01-22 11:50:37 +0900

    [feat] 룸 클래스
    1. 기본적인 룸 형태 잡기
    2. setPlayer로 유저들에게 플레이어 클래스 생성
    3. getPlayer로 플레이어 데이터 획득 가능

--- src/class/room/room.class.js
+++ src/class/room/room.class.js
@@ -1,8 +1,23 @@
+import Player from '../in-game/player.class';
+
 class Room {
-  constructor() {
-    // 속성 채우기
+  constructor(id, users) {
+    id = id;
+    this.players = new Map();
+    setPlayers(users);
   }
-  // 메서드 채우기
+
+  setPlayers(users) {
+    for (let user of users) {
+      this.players.set(user.socket, new Player(user.socket, this.id));
+    }
+  }
+
+  getPlayer(socket) {
+    if (this.players[socket] !== undefined) {
+      return this.players[socket];
+    }
+  }
 }
 
 export default Room;
