5161ae857c0a50061c443a346c1bf10f05392523
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-01-22 16:34:28 +0900

    [feat] makeMonsterPath 함수 구현

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import net from 'net';
 import onConnection from './event-listener/connect.js';
 import initServer from './init/init.server.js';
 import config from './config/configs.js';
+import makePath from './utils/path/make.monster.path.js';
 
 /* 서버 생성 */
 const server = net.createServer(onConnection);
@@ -10,6 +11,9 @@ const startServer = async () => {
   await initServer();
   server.listen(config.env.port, config.env.host, () => {
     console.log(`서버 시작!!`);
+    console.log(makePath(5));
+    console.log(makePath(1));
+    console.log(makePath(4));
   });
 };
 
