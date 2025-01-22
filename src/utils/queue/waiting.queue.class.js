01112d9ae0ac7c9c483ff2f69b77a08b526079ca
bakfox <153157605+bakfox@users.noreply.github.com>
2025-01-22 11:54:26 +0900

    [feat] 매칭 새션 클래스 제작
    1. 유저 큐에 집어넣는 시스템
    2. 유저 매칭 취소 시스템
    3. 유저 대기열에 2명 이상이면 매칭 시작
    4. 매칭 성공시 방생성

--- src/utils/queue/waiting.queue.class.js
+++ src/utils/queue/waiting.queue.class.js
@@ -1,5 +1,44 @@
+import { roomSession } from '../../session/session.js';
+
+// 매치메이킹
 class WaitingQueue {
   queue = new Set();
+  isMatching = false;
+
+  // 유저 매칭 시작시 넣어줌
+  addQueue(user) {
+    if (!this.queue.has(user)) {
+      this.queue.add(user);
+    }
+    if (this.queue > 1 && !this.isMatching) {
+      startMatch();
+    }
+  }
+  // 유저 취소 용도
+  dequeueUser(user) {
+    if (!this.queue.has(user)) {
+      this.queue.delete(user);
+    }
+  }
+  // 매칭 시작
+  startMatch() {
+    if (this.queue.size < 2) {
+      return;
+    }
+    if (!this.isMatching) {
+      this.isMatching = true;
+    }
+    for (let user of this.queue) {
+    }
+    setTimeout(startMatch, 1000);
+  }
+  //매칭 성공
+  onFoundMatch(users) {
+    for (let user of users) {
+      dequeueUser(user);
+    }
+    roomSession.addRoom(users);
+  }
 }
 
 export default WaitingQueue;
