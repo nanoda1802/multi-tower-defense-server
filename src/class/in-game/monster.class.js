20334a5b45e08901879c171cd357090ed2e24944
kimjehoon <kms5064@naver.com>
2025-01-22 14:36:32 +0900

    [feat] monster.class
    1. 생성시점, 골드(미정)

--- src/class/in-game/monster.class.js
+++ src/class/in-game/monster.class.js
@@ -7,8 +7,12 @@ class Monster {
       this.level = level;
       this.stat = monsterData.find((element) => element.Rcode === Rcode);
       this.type = ''; //'own', 'enemy' 로 표기
+      this.spawnAt = Date.now();
+      this.gold = 0; // 데이터에서 받아올지? 값을 지정해줄지?
+
     }
   
+
   }
   
 
