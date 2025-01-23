b27de4a603edf4ad23de29bd7ffec0b89cb89f27
bakfox <153157605+bakfox@users.noreply.github.com>
2025-01-23 14:26:51 +0900

    [feat] 유저 찾고 대기열에 추가
    추가했습니다!

--- src/handlers/match/add.match.handler.js
+++ src/handlers/match/add.match.handler.js
@@ -1,9 +1,11 @@
-import { waitingQueue } from '../../session/session.js';
+import { userSession, waitingQueue } from '../../session/session.js';
 
 const addMatchHandler = (socket, payload) => {
-  const user = '';
-  // 유저 찾아서 대기열에 넣어주기
-  waitingQueue.addQueue(user);
+  const user = userSession.getUser(socket);
+  if (user !== undefined) {
+    // 유저 찾아서 대기열에 넣어주기
+    waitingQueue.addQueue(user);
+  }
 };
 
 export default addMatchHandler;
