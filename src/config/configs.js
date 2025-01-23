49ddce4449a15bf896d82878577b5e5ee36392ca
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-01-23 18:03:50 +0900

    [feat] config 상수 추가

--- src/config/configs.js
+++ src/config/configs.js
@@ -1,4 +1,4 @@
-import { INITIAL_GOLD, TOWER_COST } from './constants/game.js';
+import { BASE_HP, INITIAL_GOLD, MONSTER_SPAWN_INTERVAL, TOWER_COST } from './constants/game.js';
 import userState from './constants/user.js';
 import env from './env/env.js';
 import globalFailCode from './packet/fail.code.js';
@@ -55,6 +55,8 @@ const config = {
   game: {
     initialGold: INITIAL_GOLD,
     towerCost: TOWER_COST,
+    baseHp: BASE_HP,
+    monsterSpawnInterval: MONSTER_SPAWN_INTERVAL,
   },
   userState: {
     waiting: userState.WAITING,
