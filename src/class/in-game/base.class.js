60c779afa1aa90ff68c9feb62cb54edbf4e024fe
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-01-23 19:11:46 +0900

    [fix] 핸들러 로직 구조 변경
    기존 base 메서드에서 통신을 하던 방식에서 핸들러에서 통신값을 반환하는 방식으로 변경

--- src/class/in-game/base.class.js
+++ src/class/in-game/base.class.js
@@ -1,6 +1,3 @@
-import config from "../../config/configs.js";
-import makePacketBuffer from "../../utils/send-packet/makePacket.js";
-
 class Base {
   constructor(maxHp) {
     this.gameOver = false;
@@ -8,20 +5,11 @@ class Base {
     this.hp = maxHp;
   }
 
-  damaged(room, userId, damage) {
+  damaged(damage) {
     if (this.hp - damage > 0) this.hp -= monster.atk;
     else {
-      // 게임종료 알림 패킷 생성
-      // room 내의 플레이어들에게 전달
-      room.players.forEach((player) => {
-        let packet
-        // player가 자신일 경우 패배, 다를 경우 승리 정보를 반환
-        if(player.id === userId)
-          packet = makePacketBuffer(config.packetType.gameOverNotification, { isWin: false })
-        else
-          packet = makePacketBuffer(config.packetType.gameOverNotification, { isWin: true })
-        player.socket.write(packet)
-      });
+      this.hp = 0
+      this.gameOver = true
     }
   }
 
