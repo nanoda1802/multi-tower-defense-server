61c07164398074b329ed88077f65504724beaf6c
bakfox <153157605+bakfox@users.noreply.github.com>
2025-01-24 19:03:32 +0900

    [feat] room 브로드캐스트 기능 추가
    추가했습니다!

--- src/class/room/room.class.js
+++ src/class/room/room.class.js
@@ -1,6 +1,5 @@
 import finishMatchHandler from '../../handlers/match/finish.match.handler.js';
 import Player from '../in-game/player.class.js';
-
 class Room {
   constructor(id, users) {
     this.id = id;
@@ -10,6 +9,7 @@ class Room {
     this.monsterId = 1;
     this.towerId = 1;
 
+    this.users = new Map();
     this.players = new Map();
     this.setPlayers(users);
   }
@@ -17,20 +17,33 @@ class Room {
   // 플레이어 관련
   setPlayers(users) {
     for (let user of users) {
+      this.users.set(user.id, user);
       user.enterRoom(this.id);
       this.players.set(user.id, new Player(user.id, user.socket, this.id));
     }
     finishMatchHandler(this);
   }
 
+  getUser(id) {
+    return this.users.get(id);
+  }
+
   getPlayer(id) {
     return this.players.get(id);
   }
+
   getMonsterId() {
     return this.monsterId++;
   }
+
   getTowerId() {
     return this.towerId++;
+  }
+
+  broadcast(packet) {
+    this.users.forEach((user) => {
+      user.send.write(packet);
+    });
   }
 }
 
