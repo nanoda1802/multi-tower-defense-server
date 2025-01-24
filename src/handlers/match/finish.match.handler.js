8904dc6befc1948f9b2d5a46a493b22fb2ee99ba
bakfox <153157605+bakfox@users.noreply.github.com>
2025-01-24 20:38:27 +0900

    [feat] 매치 약간 수정
    유저에서 만들어서 보내는걸로 수정

--- src/handlers/match/finish.match.handler.js
+++ src/handlers/match/finish.match.handler.js
@@ -2,12 +2,16 @@ import config from '../../config/configs.js';
 import { userSession } from '../../session/session.js';
 import makePath from '../../utils/path/make.monster.path.js';
 import makePacketBuffer from '../../utils/send-packet/makePacket.js';
-import { makeBaseData, makeGameState, makeInitialGameState } from '../../utils/send-packet/payload/game.data.js';
+import {
+  makeBaseData,
+  makeGameState,
+  makeInitialGameState,
+} from '../../utils/send-packet/payload/game.data.js';
 import { makeMatchStartNotification } from '../../utils/send-packet/payload/notification/game.notification.js';
 
 const finishMatchHandler = (room) => {
   let monsterPath = {};
-  let playerData = {}
+  let playerData = {};
   const playerId = [];
   //초기값
   const initialGameState = makeInitialGameState(
@@ -16,10 +20,10 @@ const finishMatchHandler = (room) => {
     config.game.initialGold,
     config.game.monsterSpawnInterval,
   );
-  
+
   //길만들기 // 객체 형태로 관리해 달라고 요청 하기.
   room.players.forEach((player) => {
-    monsterPath[player.id] = makePath(5)
+    monsterPath[player.id] = makePath(5);
     playerData[player.id] = makeGameState(
       player.gold,
       makeBaseData(player.base.hp, player.base.maxHp),
@@ -31,8 +35,8 @@ const finishMatchHandler = (room) => {
       monsterPath[player.id],
       monsterPath[player.id][monsterPath[player.id].length - 1],
     );
-    playerId.push(player.id)
-  })
+    playerId.push(player.id);
+  });
 
   // 전달
   room.players.forEach((player) => {
@@ -41,16 +45,8 @@ const finishMatchHandler = (room) => {
       playerData[player.id],
       playerData[playerId.find((e) => e !== player.id)],
     );
-
-    const packet = makePacketBuffer(
-      config.packetType.matchStartNotification,
-      userSession.getUser(player.socket).sequence,
-      S2CMatchStartNotification,
-    );
-
-    player.socket.write(packet);
-  })
-
+    player.user.sendPacket(config.packetType.matchStartNotification, S2CMatchStartNotification);
+  });
 };
 
 export default finishMatchHandler;
