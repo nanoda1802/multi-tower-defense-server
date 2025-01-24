6da8a3df2075f15add0f405a5de4d15a67a2b081
dwook <dwook1637@gmail.com>
2025-01-24 17:57:47 +0900

    [feat] user.sendPacket 작성

--- src/class/user/user.class.js
+++ src/class/user/user.class.js
@@ -1,3 +1,4 @@
+import { getProtoMessages } from '../../init/load.proto.js';
 import config from '../../config/configs.js';
 
 const { userState } = config;
@@ -17,7 +18,7 @@ class User {
       lose: null,
     };
     // 임시 값 저장 (테스트용 후엔 null로 변경해야함)
-    this.mmr = 100; 
+    this.mmr = 100;
     this.sequence = 1;
   }
 
@@ -54,6 +55,68 @@ class User {
 
   getSequence() {
     return this.sequence++;
+  }
+
+  sendPacket(packetType, payload) {
+    //패킷타입 (number -> string)
+    const packetTypeValues = Object.values(config.packetType);
+    const packetTypeIndex = packetTypeValues.findIndex((f) => f === packetType);
+    const packeTypeName = Object.keys(config.packetType)[packetTypeIndex];
+
+    // 페이로드
+    const proto = getProtoMessages().GamePacket;
+    const message = proto.create({ [packeTypeName]: payload });
+    const payloadBuffer = proto.encode(message).finish();
+
+    // 헤더 필드값
+    const version = config.env.clientVersion || '1.0.0';
+    const versionLength = version.length;
+    const payloadLength = payloadBuffer.length;
+
+    if (true) {
+      // 콘솔로그 필터링하려면 조건 입력
+      console.log('------------- 주는 값 -------------');
+      console.log(`type: ${packetType}.${packeTypeName}`);
+      console.log('versionLength:', versionLength);
+      console.log('version:', version);
+      console.log('sequence:', this.sequence);
+      console.log('payloadLength:', payloadLength);
+      console.log('payload:', message);
+      console.log('-------------------------------');
+    }
+
+    // 헤더 필드 - 패킷 타입
+    const packetTypeBuffer = Buffer.alloc(2);
+    packetTypeBuffer.writeUint16BE(packetType, 0);
+
+    // 헤더 필드 - 버전 길이
+    const versionLengthBuffer = Buffer.alloc(1);
+    versionLengthBuffer.writeUInt8(versionLength, 0);
+
+    // 헤더 필드 - 버전
+    const versionBuffer = Buffer.from(version);
+
+    // 헤더 필드 - 시퀀스
+    const sequenceBuffer = Buffer.alloc(4);
+    sequenceBuffer.writeUint32BE(this.sequence, 0);
+
+    // 헤더 필드 - 페이로드 길이
+    const payloadLengthBuffer = Buffer.alloc(4);
+    payloadLengthBuffer.writeUInt32BE(payloadLength, 0);
+
+    // 헤더
+    const headerBuffer = Buffer.concat([
+      packetTypeBuffer,
+      versionLengthBuffer,
+      versionBuffer,
+      sequenceBuffer,
+      payloadLengthBuffer,
+    ]);
+
+    // 패킷
+    const packetBuffer = Buffer.concat([headerBuffer, payloadBuffer]);
+
+    this.socket.write(packet);
   }
 }
 
