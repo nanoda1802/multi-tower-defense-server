85f0f781232fbfaec8ef9ce39a6ec5dc1a251c9a
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-01-24 17:26:18 +0900

    [feat] road Sync 구현완료 + base Postion 정상화

--- src/utils/path/make.monster.path.js
+++ src/utils/path/make.monster.path.js
@@ -3,14 +3,14 @@ const makePath = (count) => {
   // 만약 count가 2개 이하이면 기본값 직선으로 반환
   if (count <= 2)
     return [
-      {x: 0, y: 350},
-      {x: 1500,y: 350},
+      {x: 0, y: 300},
+      {x: 1350,y: 300},
     ];
-  const weightX = Math.trunc(1500 / (count - 1));
+  const weightX = Math.trunc(1350 / (count - 1));
 
   for (let i = 0; i < count; i++) {
     let x = weightX * i;
-    if (i === count - 1) x = 1500;
+    if (i === count - 1) x = 1350;
     const y = Math.trunc(Math.random() * 200) + 200;
     
     paths.push({x, y});
