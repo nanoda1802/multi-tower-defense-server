acf953dc915e9f789e307d710ea6d3110654f86a
조용필 <78397393+bbie-6772@users.noreply.github.com>
2025-01-31 21:59:04 +0900

    [fix] 매칭 전 유저 정보 확인

--- src/class/queue/waiting.queue.class.js
+++ src/class/queue/waiting.queue.class.js
@@ -10,7 +10,7 @@ class WaitingQueue {
   /* 유저를 대기열에 투입시키는 메서드 */
   enqueueUser(user) {
     // [1] 이미 대기열에 있는 유저인지 확인
-    if (!this.queue.has(user)) {
+    if (!this.queue.has(user) && user.id !== null) {
       // [1-1] 아니라면 대기열에 유저를 넣어주고, 유저의 state를 "matchMaking"으로 변경
       this.queue.add(user);
       user.matchMaking();
